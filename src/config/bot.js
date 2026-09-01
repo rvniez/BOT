826d072fb09000b5ddee20295898c3d7627959b2
rvniez <angelake091103@gmail.com>
2026-09-01 20:17:38 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#4a0000",
+      secondary: "#b9b9b9",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
